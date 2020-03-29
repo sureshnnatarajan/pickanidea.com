b9efb2bcc970af2aef25f42963555a39ed257291
Suresh Natarajan <34959692+sureshnnatarajan@users.noreply.github.com>
2020-03-29 07:41:49 +0530

    Develop (#2)
    * Added card layout. Altered few wordings
    
    * UI layout changes and image changes
    
    Co-authored-by: Suresh Natarajan <sureshnatarajan@Sureshs-MacBook-Pro.local>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pickanidea-com';
+  //images = ['./assets/bg-masthead.jpg','./assets/6895187159_2f1cf391a3_w.jpg', './assets/5430683205_d58b1df36c_w.jpg', './assets/7184081947_a6d48f3f93_w.jpg'];
+  //images = ['./assets/bg-masthead.jpg'];
+  //images = ['./assets/white-background.png'];
+
+  constructor(private modalService: NgbModal) {}
+
+  closeResult = '';
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed.`;
+    });
+  }
 }
